5f2b24bc99aee88de7489285b62fd38acefd1de4
SHREYANSH <137887705+02shreyansh@users.noreply.github.com>
2024-09-20 14:19:41 +0530

    Update Login.jsx

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,6 +1,6 @@
 import React, {useState,useEffect} from 'react'
 import {Link, useNavigate} from 'react-router-dom'
-import { login as authLogin } from '../store/authSlice'
+import { login as authLogin } from '../store/AuthSlice'
 import {Button, Input, Logo} from "./index"
 import {useDispatch, useSelector} from "react-redux"
 import authService from "../appwrite/auth"
